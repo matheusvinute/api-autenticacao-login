c143d0fdfce3f75644c51330ded16436e5546f08
Matheus Vinute <36545074+matheusvinute@users.noreply.github.com>
2022-06-15 08:08:53 -0300

    Delete dist directory

--- dist/settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RABBIT_DSN = exports.RABBIT_PORT = exports.RABBIT_PASSWORD = exports.RABBIT_USER = exports.RABBIT_HOST = exports.IS_DEVELOPMENT = void 0;
-exports.IS_DEVELOPMENT = true;
-exports.RABBIT_HOST = 'amqp://localhost';
-exports.RABBIT_USER = '';
-exports.RABBIT_PASSWORD = '';
-exports.RABBIT_PORT = 5672;
-exports.RABBIT_DSN = exports.IS_DEVELOPMENT ? `${exports.RABBIT_HOST}` : ``;
